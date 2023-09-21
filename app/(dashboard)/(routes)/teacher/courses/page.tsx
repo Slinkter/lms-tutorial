b9461ef8e07b5e20f9d7dab61473519326aacd63
Slinkter <luis.j.cueva@gmail.com>
2023-09-21 18:52:02 -0500

    testing

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursePage = () => {
-  return <div>course Page !</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher">
+        <Button>new Course</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default CoursePage;
